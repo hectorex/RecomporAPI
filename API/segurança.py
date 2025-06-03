6b92e1b9a40ccf82667b229d1743cec080079e4b
hectorex <celsohectorhm@gmail.com>
2025-06-03 08:49:15 -0400

    password_check: verificando força da senha

--- API/segurança.py
+++ API/segurança.py
@@ -7,3 +7,20 @@ def get_password_hash(password: str):
 
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
+
+def password_check(password: str):
+    spclCaracs = ["!","@","#","$","%","&","*"]
+    strong = True
+    if len(password) < 6:
+        strong = False
+    if len(password) > 20:
+        strong = "Erro aqui"
+    if any(char.isdigit() for char in password) == False:
+        strong = False
+    if any(char.isupper() for char in password) == False:
+        strong = False
+    if any(char.islower() for char in password) == False:
+        strong = False
+    if any(char in spclCaracs for char in password) == False:
+        strong == False
+    return strong
