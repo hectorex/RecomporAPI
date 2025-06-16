b5273d71bd5036381f59e8cb300b4731d587af66
mnzlipe <luizfelipemam2007@gmail.com>
2025-06-16 11:26:54 -0400

    alembic + inicio do db true

--- poetry_recomporapi/API/models/user.py
+++ poetry_recomporapi/API/models/user.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 @table_registry.mapped_as_dataclass
 class User:
-    __tablename__ = "user"
+    __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(init=False, primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
@@ -15,3 +15,6 @@ class User:
     created_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now()
     )
+
+
+    updated_at: Mapped[datetime] = mapped_column(onupdate=func.now()) #adicionei agr
