db6255379263a9413edba72230d300b41536f76b
hectorex <celsohectorhm@gmail.com>
2025-06-16 09:52:18 -0400

    funcionando testes

--- poetry_recomporapi/API/schemas/compostagem_schema.py
+++ poetry_recomporapi/API/schemas/compostagem_schema.py
@@ -1,16 +1,23 @@
 from pydantic import BaseModel, field_validator
 from datetime import datetime
 
+def calculo_previsao(quantReduo: float) -> int:
+        dias = int(quantReduo / 0.5)
+        return dias #a cada 500g (0,5kg) leva 1 dia, minimo de 7 dias
+        # ele vai retornar isso numa futura rota de post
+
 class DadosCompostagem(BaseModel):
     nome: str
-    data: str
-    peso: float
-    previsao: int
+    data_compostagem: str
+    quantReduo: float
+    frequencia: str
 
-    @field_validator('data', mode='before')
-    def validar_data(cls, data):
+    @field_validator('data_compostagem', mode='before')
+    def validar_data(cls, data_compostagem):
         try:
-            datetime.strptime(data, "%d/%m/%Y")
-            return data
+            datetime.strptime(data_compostagem, "%d/%m/%Y")
+            return data_compostagem
         except ValueError:
             raise ValueError("Use o formato DD/MM/YYYY.")
+        
+    
