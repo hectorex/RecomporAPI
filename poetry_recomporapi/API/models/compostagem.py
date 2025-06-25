b79302a3d8a103f695dcd9e6b1a47e3c060064de
mnzlipe <luizfelipemam2007@gmail.com>
2025-06-25 10:09:52 -0400

    padronizado nomes dos modelos a fim de corrigir erros

--- poetry_recomporapi/API/models/compostagem.py
+++ poetry_recomporapi/API/models/compostagem.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 @table_registry.mapped_as_dataclass
 class Compostagem:
-    __tablename__ = "compostagem" #Criando tabela compostagem
+    __tablename__ = "compostagens_table" #Criando tabela compostagem
 
     id: Mapped[int] = mapped_column(init= False, primary_key=True) #definindo ID como PK 
     nome: Mapped[str] = mapped_column()
