e43066db21a0827ed7a2dee9a0863a8328c9a387
hectorex <celsohectorhm@gmail.com>
2025-06-25 10:38:27 -0400

    finalizacao geracao ids automaticos

--- poetry_recomporapi/API/models/user_model.py
+++ poetry_recomporapi/API/models/user_model.py
@@ -7,12 +7,12 @@ from uuid import uuid4
 
 @table_registry.mapped_as_dataclass
 class User:
-    __tablename__ = "user" #Criando tabela user
+    __tablename__ = "users_table" #Criando tabela user
 
-    id: Mapped[str] = mapped_column(default=lambda: str(uuid4()), primary_key=True) #definindo ID como PK
     username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str] #pode se repetir 
     email: Mapped[str] = mapped_column(unique=True)
     created_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now()
     )
+    id: Mapped[str] = mapped_column(primary_key=True, default_factory=lambda: str(uuid4())) #definindo ID como PK
