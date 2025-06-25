2843b9f3d409688b492c4634d93d65eaa37d6e01
mnzlipe <luizfelipemam2007@gmail.com>
2025-06-25 09:50:37 -0400

    consertado erro no banco de dados envolvendo o gitignore

--- poetry_recomporapi/API/models/user_model.py
+++ poetry_recomporapi/API/models/user_model.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 @table_registry.mapped_as_dataclass
 class User:
-    __tablename__ = "user" #Criando tabela user
+    __tablename__ = "users_table" #Criando tabela user
 
     id: Mapped[int] = mapped_column(init=False, primary_key=True) #definindo ID como PK
     username: Mapped[str] = mapped_column(unique=True)
