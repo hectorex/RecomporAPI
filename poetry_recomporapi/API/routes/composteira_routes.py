d4ac6b934822a7f4dd94f4783f24c8d657c0510d
hectorex <celsohectorhm@gmail.com>
2025-07-14 09:28:35 -0400

    att: validacao de tamanho composteira

--- poetry_recomporapi/API/routes/composteira_routes.py
+++ poetry_recomporapi/API/routes/composteira_routes.py
@@ -20,11 +20,23 @@ def criar_composteira(user_id: str,composteira: DadosComposteira, session = Depe
     )
 
     if db_composteira:
-            raise HTTPException(
-                status_code=HTTPStatus.CONFLICT,
-                detail='Composteira já existe com esse nome.',
+        raise HTTPException(
+            status_code=HTTPStatus.CONFLICT,
+            detail='Composteira já existe com esse nome.',
             )
-        
+    
+    db_composteira = session.scalar(
+        select(Composteira).where(
+            (Composteira.tamanho <= 0)
+        )
+    )
+
+    if db_composteira:
+        raise HTTPException(
+            status_code=400,
+            detail="O tamanho inserido é inválido, insira um número maior que 0."
+        )
+           
     
     db_user = session.scalar(
         select(User).where(
