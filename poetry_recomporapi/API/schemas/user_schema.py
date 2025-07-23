a7891d02c92792e94135b9ab5787120a25d68674
hectorex <celsohectorhm@gmail.com>
2025-07-23 09:19:24 -0400

    att: validacao de email

--- poetry_recomporapi/API/schemas/user_schema.py
+++ poetry_recomporapi/API/schemas/user_schema.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from uuid import uuid4
 class DadosUser(BaseModel): #Classe do user
     username: str
     password: str
-    email: str
+    email: EmailStr
 
 class DadosSenha(BaseModel): #Classe da senha
     user_id: uuid4
