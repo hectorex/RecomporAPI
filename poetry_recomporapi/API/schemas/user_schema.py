d2211492143be3ba75b977109868be41965989c2
mnzlipe <luizfelipemam2007@gmail.com>
2025-07-23 09:20:56 -0400

    email validator

--- poetry_recomporapi/API/schemas/user_schema.py
+++ poetry_recomporapi/API/schemas/user_schema.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from uuid import uuid4
 class DadosUser(BaseModel): #Classe do user
     username: str
     password: str
-    email: str
+    email: EmailStr #validar o email se ele eh um email msm
 
 class DadosSenha(BaseModel): #Classe da senha
     user_id: uuid4
