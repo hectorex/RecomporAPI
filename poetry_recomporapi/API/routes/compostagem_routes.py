6a1b85ed121acb743e416466c27c1eba609a9c87
hectorex <celsohectorhm@gmail.com>
2025-08-01 07:52:50 -0400

    att: edicao de composteira e compostagem

--- poetry_recomporapi/API/routes/compostagem_routes.py
+++ poetry_recomporapi/API/routes/compostagem_routes.py
@@ -12,8 +12,6 @@ from API.database import get_session
 from API.models.compostagem import Compostagem
 from API.models.composteira import Composteira
 from API.schemas.compostagem_schema import DadosCompostagem
-# from API.database.fake_db import bd_compostagens, bd_composteiras
-
 
 
 router =  APIRouter()
@@ -21,6 +19,15 @@ router =  APIRouter()
 @router.post("/minhas_composteiras/{composteira_id}/criar_compostagem") # criar compostagem
 async def criar_compostagem(composteira_id: str, compostagem: DadosCompostagem, session = Depends(get_session)): #criação da session
 
+    db_composteira = session.scalar(
+    select(Composteira).where(Composteira.id == composteira_id)
+)
+    if not db_composteira:
+        raise HTTPException(
+            status_code=404, 
+            detail="Composteira não encontrada."
+            )
+    
     if len(compostagem.nome) < 3 and compostagem.nome != "   ":
         raise HTTPException(
             status_code=400,
@@ -47,18 +54,9 @@ async def criar_compostagem(composteira_id: str, compostagem: DadosCompostagem, 
     if db_compostagem:
         raise HTTPException( #verificando se o nome existe no db
             status_code=HTTPStatus.CONFLICT, 
-            detail="Nome já existente"
+            detail="Valor inválido. Nome já existente"
             )
     
-    db_composteira = session.scalar(
-    select(Composteira).where(Composteira.id == composteira_id)
-)
-    if not db_composteira:
-        raise HTTPException(
-            status_code=404, 
-            detail="Composteira não encontrada."
-            )
-   
     db_compostagem = Compostagem( # Instanciando objeto da classe compostagem
         nome= compostagem.nome,
         data_compostagem= compostagem.data_compostagem,
@@ -70,12 +68,13 @@ async def criar_compostagem(composteira_id: str, compostagem: DadosCompostagem, 
     session.add(db_compostagem)
     session.commit()
     session.refresh(db_compostagem)
+
     return db_compostagem
 
 @router.get('/minhas_composteiras/{composteira_id}/minhas_compostagens') #listando compostagens
 def get_compostagens(limit: int = 10, offset: int = 0, session: Session = Depends(get_session)):
     compostagens = list(session.scalars(select(Compostagem).limit(limit).offset(offset)))
-    if len(compostagens) == 0:
+    if len(compostagens) == 0: #verificando se a tabela de compostagens está vazia
         return {"message": "Nenhuma composteira encontrada."}
     else:
         return {"compostagens_table": [asdict(c) for c in compostagens]}
@@ -96,12 +95,35 @@ def delete_compostagem(id: str, session: Session = Depends(get_session)):
 @router.put("/minhas_composteiras/{composteira_id}/minhas_compostagens/{id}") #editar uma composteira ja existente
 def update_compostagem(id: str, compostagem: DadosCompostagem, session: Session = Depends(get_session)):
     db_compostagem = session.scalar(select(Compostagem).where(Compostagem.id == id))
-
+    
+    if len(compostagem.nome) < 3 and compostagem.nome != "   ":
+        raise HTTPException(
+            status_code=400,
+            detail="Valor inválido. Insira: um valor com pelo menos 3 caracteres."
+        )
+    
+    if not compostagem.quantReduo > 0:
+        raise HTTPException( #verificando se a quantReduo possui valor válido
+            status_code=400,
+            detail="Valor inválido. Insira: um valor maior que 0."
+        )
     if compostagem.frequencia.capitalize() not in ["Diaria","Semanal","Mensal"]:
         raise HTTPException( #verificando se frequencia é válida
             status_code=400, 
             detail="Valor inválido. Insira: Diaria, Semanal ou Mensal (sem acento)."
-            )    
+            )
+
+        
+    db_compostagem = session.scalar(
+        select(Compostagem).where(
+            (Compostagem.nome == compostagem.nome )
+        )
+    )
+    if db_compostagem:
+        raise HTTPException( #verificando se o nome existe no db
+            status_code=HTTPStatus.CONFLICT, 
+            detail="Valor inválido. Nome já existente"
+            )  
 
     if not db_compostagem:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Compostagem não encontrada.")
--- poetry_recomporapi/API/routes/composteira_routes.py
+++ poetry_recomporapi/API/routes/composteira_routes.py
@@ -111,6 +111,29 @@ def update_composteira(id: str, composteira: DadosComposteira, session: Session 
 
     if not db_composteira:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Composteira não encontrada.")
+    
+    if len(composteira.nome) < 3 and composteira.nome != "   ":
+        raise HTTPException(
+            status_code=400,
+            detail="Valor inválido. Insira: um valor com pelo menos 3 caracteres. Não insira: 3 espaços em branco."
+        )
+    if composteira.tamanho <= 0: #verificando se o tamanho é válido
+        raise HTTPException(
+            status_code=400,
+            detail="O tamanho inserido é inválido, insira um número maior que 0."
+        )
+
+    db_composteira = session.scalar( #consultando se tem uma composteira com mesmo nome no banco
+        select(Composteira).where(
+            (Composteira.nome == composteira.nome)
+        )
+    )
+
+    if db_composteira:
+        raise HTTPException(
+            status_code=HTTPStatus.CONFLICT,
+            detail='Composteira já existe com esse nome.',
+            )
 
     try:
         db_composteira.nome = composteira.nome
